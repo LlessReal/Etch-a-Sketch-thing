e1f0b639630893efb01840c64c1336b9338fffe4
LlessReal <marshall3001.mm@gmail.com>
2025-01-09 14:48:04 -0500

    Made a new functionallity that adds grids

--- script.js
+++ script.js
@@ -3,47 +3,93 @@ const body = document.querySelector("body")
 body.setAttribute("style",`
     background-color: cyan; 
     min-height: 100vh;
+    min-width: 100%;
     display: flex; 
-    justify-content: center; align-items: center; 
+    flex-direction: column;
     margin: 0px;
     `)
+
+TotalBoxContainer = document.createElement("div")
+TotalBoxContainer.setAttribute("style",`display: flex; flex-direction: column;
+    align-items: center;`)
+body.appendChild(TotalBoxContainer)
+
+NewSetButton = document.createElement("button")
+NewSetButton.textContent = "Create new grid"
+DefaultButtonStyle = function() {
+    NewSetButton.setAttribute("style",`padding: 10px; margin: 20px;
+        color: white; background-color: red;
+        border: groove 10px white; border-radius: 20px;
+        transition: 0.5s
+        `)
+
+}
+DefaultButtonStyle()
+NewSetButton.addEventListener("mouseover", () => {
+    NewSetButton.setAttribute("style",`
+        box-shadow: 0 0 40px yellow inset;
+        padding: 10px; margin: 20px;
+        color: white; background-color: red;
+        border: groove 10px white; border-radius: 20px;
+        transition: 0.5s;
+        cursor: pointer;`)
+})
+NewSetButton.addEventListener("mouseout", () => {
+    DefaultButtonStyle()
+})
+GridSize = 16
+
+TotalBoxContainer.appendChild(NewSetButton)
 
 BoxContainer = document.createElement("div")
 BoxContainer.setAttribute("style",`display: flex;
     padding: 10px;
-    width: 50%; 
+    width: %;
+    max-height: 20px;
     flex-wrap: wrap;`)
-body.appendChild(BoxContainer)
+TotalBoxContainer.appendChild(BoxContainer)
 
-Box1 = document.createElement("div"); BoxContainer.appendChild(Box1);
-Box2 = document.createElement("div"); BoxContainer.appendChild(Box2);
-Box3 = document.createElement("div"); BoxContainer.appendChild(Box3);
-Box4 = document.createElement("div"); BoxContainer.appendChild(Box4);
-Box5 = document.createElement("div"); BoxContainer.appendChild(Box5);
-Box6 = document.createElement("div"); BoxContainer.appendChild(Box6);
-Box7 = document.createElement("div"); BoxContainer.appendChild(Box7);
-Box8 = document.createElement("div"); BoxContainer.appendChild(Box8);
-Box9 = document.createElement("div"); BoxContainer.appendChild(Box9);
-Box10 = document.createElement("div"); BoxContainer.appendChild(Box10);
-Box11 = document.createElement("div"); BoxContainer.appendChild(Box11);
-Box12 = document.createElement("div"); BoxContainer.appendChild(Box12);
-Box13 = document.createElement("div"); BoxContainer.appendChild(Box13);
-Box14 = document.createElement("div"); BoxContainer.appendChild(Box14);
-Box15 = document.createElement("div"); BoxContainer.appendChild(Box15);
-Box16 = document.createElement("div"); BoxContainer.appendChild(Box16);
-
-BoxContainer.querySelectorAll("div").forEach((item, index) => {
-    item.setAttribute("style",`background-color: gray; 
-        font-size: 20px; 
-        padding: 20px;
-        margin: 10px;
-        flex: 1;
-        justify-content: center;  
-        border: solid 5px;
-        border-radius: 20 px;
-        `)
-    item.textContent = `Box ${index + 1}` 
+NewSetButton.addEventListener("click", () => {
+    GridSize = prompt("How large do you want your new grid to be (put in the size of the sides by grid #")
+    BoxContainer.innerHTML = ""
+    CreateNewGridFunc()
 })
+
+CreateNewGridFunc = function() {
+    for (i = 0; i < (GridSize * GridSize); i++) {
+        NewBox = document.createElement("div")
+        BoxContainer.appendChild(NewBox);
+    }
+    
+    BoxContainer.querySelectorAll("div").forEach((item, index) => {
+        DefaultEvent = function() {
+            item.setAttribute("style",`background-color: gray; 
+            font-size: 20px; 
+            flex: 1 1 100%;
+            border: solid 3px;
+            transition: 4s;
+            max-width: 30px;
+            user-select: none;
+            `)   
+        }
+        DefaultEvent()
+        item.addEventListener("mouseover", () => {
+            item.setAttribute("style",`background-color: red; 
+            font-size: 20px; 
+            flex: 1 1 100%;
+            border: solid 3px;
+            max-width: 30px;
+            user-select: none;
+            `)
+            
+        })
+        item.addEventListener("mouseout", DefaultEvent)
+        item.textContent = `${index + 1}` 
+    })
+}
+
+CreateNewGridFunc()
+
 
 
 
