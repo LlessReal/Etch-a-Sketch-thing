16cf7f7b42a9461a4aa38860c6013ae01970ad60
LlessReal <marshall3001.mm@gmail.com>
2025-01-10 21:01:55 -0500

    Finished script, now adding comments

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const body = document.querySelector("body") // Selects body
 body.setAttribute("style",`
     background-color: cyan; 
     min-height: 100vh;
-    min-width: 1200px;
+    min-width: 100vh;
     display: flex; 
     flex-direction: column;
+    justify-content: center; align-items: center;
     margin: 0px;
     `)
     // Idk why i set the min width like this
@@ -14,46 +15,33 @@ TotalBoxContainer = document.createElement("div")
 TotalBoxContainer.setAttribute("style",`display: flex; flex-direction: column;
     align-items: center;
     width: 960px;
-    margin-left: auto;
-    margin-right: auto;
+    
     `) // This was supposed to sensor the shit
 body.appendChild(TotalBoxContainer) // Add to body
 
 NewSetButton = document.createElement("button")
 NewSetButton.textContent = "Create new grid"
-DefaultButtonStyle = function() {
-    NewSetButton.setAttribute("style",`padding: 10px; margin: 20px;
-        color: white; background-color: red;
-        border: groove 10px white; border-radius: 20px;
-        transition: 0.5s
-        `)
+NewSetButton.setAttribute("style",`padding: 10px; margin: 20px;
+    color: white; background-color: red;
+    border: groove 10px white; border-radius: 20px;
+    transition: 0.5s;
+    width: 500px;
+    cursor: pointer;
+    `)
 
-}
-DefaultButtonStyle()
-NewSetButton.addEventListener("mouseover", () => {
-    NewSetButton.setAttribute("style",`
-        box-shadow: 0 0 40px yellow inset;
-        padding: 10px; margin: 20px;
-        color: white; background-color: red;
-        border: groove 10px white; border-radius: 20px;
-        transition: 0.5s;
-        cursor: pointer;`)
-})
-NewSetButton.addEventListener("mouseout", () => {
-    DefaultButtonStyle()
-})
-GridSize = 16
+NewSetButton.addEventListener("mouseover", () => { NewSetButton.style.boxShadow = `0 0 40px yellow inset` })
+NewSetButton.addEventListener("mouseout", () => { NewSetButton.style.boxShadow = `` })
 
 TotalBoxContainer.appendChild(NewSetButton)
 
 BoxContainer = document.createElement("div")
 BoxContainer.setAttribute("style",`display: flex;
     padding: 10px;
-    width: 960px;
-    max-height: 20px;
-    flex-wrap: wrap;`)
+    width: 900px;
+    flex-wrap: wrap;`) // Added wrap with 900px so it can be 16 a row
 TotalBoxContainer.appendChild(BoxContainer)
 
+GridSize = 16
 NewSetButton.addEventListener("click", () => {
     GridSize = prompt("How large do you want your new grid to be (put in the size of the sides by grid #")
     BoxContainer.innerHTML = ""
@@ -67,28 +55,20 @@ CreateNewGridFunc = function() {
     }
     
     BoxContainer.querySelectorAll("div").forEach((item, index) => {
-        DefaultEvent = function() {
-            item.setAttribute("style",`background-color: gray; 
-            font-size: 20px; 
-            flex: 1 1 auto;
+        item.setAttribute("style",`background-color: gray; 
+            font-size: 20px; text-align: center;
+            padding: 10px;
+            max-width: 30px; flex: 1 1 100%; 
             border: solid 3px;
-            transition: 4s;
             user-select: none;
-            `)   
-        }
-        DefaultEvent()
+            `)  // Properties given to boxes
         item.addEventListener("mouseover", () => {
-            item.setAttribute("style",`background-color: red; 
-            font-size: 20px; 
-            flex: 1 1 auto;
-            border: solid 3px;
-            user-select: none;
-            `)
-            
+            item.style.transition = "0s"
+            item.style.backgroundColor = "red" // Immediately turns red when hovered over
         })
         item.value = 10
-        item.addEventListener("mouseout", DefaultEvent) // Runs default event (background color will get replaced next tho)
         item.addEventListener("mouseout", () => {
+            item.style.transition = "4s"
             item.style.backgroundColor = `rgb(${127 + Math.random() * 127},${127 + Math.random() * 127},${127 + Math.random() * 127})`
             // Background color becomes something new (with 127 starting value for more brightness)
             if (item.value != 0) {
@@ -101,7 +81,6 @@ CreateNewGridFunc = function() {
         item.textContent = `${index + 1}` // Numbers given for each number cuz why not
     })
 }
-
 CreateNewGridFunc()
 
 
